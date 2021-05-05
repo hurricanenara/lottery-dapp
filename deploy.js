a04e6b9fe2f6d2cc377c158da17e7f4347c5e374
naracodes <naralee0124@gmail.com>
2021-05-05 10:54:11 -0400

    connect web3 to frontend

--- deploy.js
+++ deploy.js
@@ -17,6 +17,8 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: "1000000", from: accounts[0] });
 
+  console.log(interface);
+
   console.log("Contract deployed to", result.options.address);
 };
 
--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manager: "",
+    };
+  }
+  // web3.eth.getAccounts().then(console.log);
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    this.setState({ manager });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Lotter Contract</h3>
+        <p>This contract is managed by {this.state.manager}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
